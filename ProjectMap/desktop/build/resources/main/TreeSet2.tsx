c32d1fce57e4388a9ffbee737a8581fe5521ff2e
Gabriel Coulson <80282302+RobottheRobot@users.noreply.github.com>
2022-12-08 18:15:58 -0600

    Add files via upload
    Idk if this is what ya'll were looking for or not but thats what I made so far. It's 1920x1080 pixels so I think that's what you asked? Lmk if I should change anything.

---
+++ ProjectMap/desktop/build/resources/main/TreeSet2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="TreeSet2" tilewidth="40" tileheight="40" tilecount="144" columns="12">
+ <image source="../../Pixel Art Top Down - Basic/Texture/TX Plant.png" width="512" height="512"/>
+</tileset>
