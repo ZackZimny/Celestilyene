31c4517b40b989a6f2207b835b70e8b1514864e6
ZackZimny <zzimny9@gmail.com>
2022-12-29 21:18:56 -0600

    Delete ProjectMap directory
    These files will not be used in the ultimate production of the game.

--- ProjectMap/assets/GrassLand.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="GrassSet" tilewidth="32" tileheight="32" tilecount="64" columns="8">
- <image source="../../Pixel Art Top Down - Basic/Texture/TX Tileset Grass.png" width="256" height="256"/>
-</tileset>
--- ProjectMap/assets/GrassSet2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="GrassSet2" tilewidth="40" tileheight="40" tilecount="36" columns="6">
- <image source="../../Pixel Art Top Down - Basic/Texture/TX Tileset Grass.png" width="256" height="256"/>
-</tileset>
