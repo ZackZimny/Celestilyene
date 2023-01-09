1189ba1887d250e42cc4d68782e0defc80007c3c
ZackZimny <zzimny9@gmail.com>
2023-01-08 23:08:14 -0600

    Add files via upload

---
+++ Celestilyne/assets/Tileset.tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Tileset" tilewidth="64" tileheight="64" tilecount="6" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="BabyDragonBlue-1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="Player-1.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="Baby Dragon-1.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="32" source="Evil Mage-1.png"/>
+ </tile>
+ <tile id="4">
+  <image width="64" height="64" source="Tree.png"/>
+ </tile>
+ <tile id="5">
+  <image width="32" height="32" source="Slime.png"/>
+ </tile>
+</tileset>
